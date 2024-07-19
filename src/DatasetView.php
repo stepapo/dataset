ab0d3e355c1b281286f3841c101ada25b84c99cf
Štěpán Postler <stepan.postler@fsv.cvut.cz>
2024-07-19 14:29:53 +0200

    wip

--- src/DatasetView.php
+++ src/DatasetView.php
@@ -62,6 +62,7 @@ class DatasetView extends Schematic implements View
 
 	#[KeyProperty] public string $name;
 	public ?string $label = null;
+	public bool $hide = false;
 	public string $datasetTemplate = self::VIEWS['list']['datasetTemplate'];
 	public string $itemListTemplate = self::VIEWS['list']['itemListTemplate'];
 	public string $itemTemplate = self::VIEWS['list']['itemTemplate'];
